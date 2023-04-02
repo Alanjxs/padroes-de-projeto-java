d6982bbc21fffbc1af6c76d488a701e84128360c
Alan JXS <alanjxs@gmail.com>
2023-04-02 08:57:40 -0300

    Implementações de exemplo Singleton

---
+++ src/padroes/projeto/java/singleton/SingletonEager.java
@@ -0,0 +1,15 @@
+package padroes.projeto.java.singleton;
+
+public class SingletonEager {
+	
+private static SingletonEager instancia = new SingletonEager();;
+	
+	private SingletonEager() {
+		super();
+	}
+	
+	public static SingletonEager getInstancia() {
+		return instancia;
+	}
+
+}
---
+++ src/padroes/projeto/java/singleton/SingletonLazy.java
@@ -0,0 +1,18 @@
+package padroes.projeto.java.singleton;
+
+public class SingletonLazy {
+	
+	private static SingletonLazy instancia;
+	
+	private SingletonLazy() {
+		super();
+	}
+	
+	public static SingletonLazy getInstancia() {
+		if (instancia == null) {
+			instancia = new SingletonLazy();
+ 		}
+		return instancia;
+	}
+
+}
